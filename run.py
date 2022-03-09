54359e8b2f966de3b9f9adac762aecafdf54a538
Ansh Dadwal <68729523+T-Dynamos@users.noreply.github.com>
2022-03-09 20:30:16 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+def match(img,tobematchedimg):
+	import cv2
+	import numpy as np
+	img_rgb = cv2.imread(img)
+	img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+	template = cv2.imread(tobematchedimg)
+	w, h = template.shape[::-1]
+	res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+	threshold = 0.8
+	loc = np.where( res >= threshold)
+	for pt in zip(*loc[::-1]):
+		cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
+	cv2.imshow('Detected',img_rgb)
+match("A.png","test_s.jpg")
